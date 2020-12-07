0e7234303057e18d6dde79fa3272ab516dea8233
Nathan Wolf <nathan@elmakers.com>
2020-12-07 12:05:04 -0800

    Create a basic plugin that does nothing but force Magic to load earlier

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MagicStartup
+
 An add-on plugin to make the Magic plugin load at startup
+
+This is usually only needed if you are using Magic's world generator on one of the default worlds.
---
+++ src/main/java/com/elmakers/mine/bukkit/magicstartup/MagicStartupPlugin.java
@@ -0,0 +1,11 @@
+package com.elmakers.mine.bukkit.magicstartup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MagicStartupPlugin extends JavaPlugin {
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+}
